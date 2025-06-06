be5e6cb5ecb11ea78bb476d54d0b2bd16f9f2b98
Mewski <mewski@mewski.dev>
2025-06-05 19:02:22 -0500

    chore: fix zephyrus

--- modules/hosts/zephyrus/configuration.nix
+++ modules/hosts/zephyrus/configuration.nix
@@ -27,7 +27,6 @@
           "pcie_aspm.policy=powersupersave"
           "intel_iommu=on"
           "iommu=pt"
-          "mem_sleep_default=deep"
         ];
 
         loader = {
@@ -83,18 +82,6 @@
         blueman.enable = true;
         udisks2.enable = true;
 
-        resolved = {
-          enable = true;
-          settings.Resolve = {
-            DNSOverTLS = "opportunistic";
-            FallbackDNS = [
-              "1.1.1.1#cloudflare-dns.com"
-              "1.0.0.1#cloudflare-dns.com"
-              "2606:4700:4700::1111#cloudflare-dns.com"
-              "2606:4700:4700::1001#cloudflare-dns.com"
-            ];
-          };
-        };
         udev.packages = [
           pkgs.probe-rs-tools
           pkgs.python3Packages.chipwhisperer
