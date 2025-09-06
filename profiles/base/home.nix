9e3c6ebf5dd00043a95d16ef11d405a06af0d278
Mewski <mewski813@gmail.com>
2025-09-05 23:12:59 -0500

    refactor(home): rearrange imports

--- profiles/base/home.nix
+++ profiles/base/home.nix
@@ -3,8 +3,9 @@
 {
   imports = [
     # Terminal applications
-    ../../modules/home-manager/apps/terminal/kitty.nix
+    ../../modules/home-manager/apps/git/git.nix
     ../../modules/home-manager/apps/neovim/neovim.nix
+    ../../modules/home-manager/apps/terminal/kitty.nix
 
     # Window manager configuration
     ../../modules/home-manager/window-manager/hyprland.nix
