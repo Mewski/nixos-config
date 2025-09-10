9b00236c6f3fd6e259e99063e1deda9fa4602f48
Mewski <mewski813@gmail.com>
2025-09-10 09:22:03 -0500

    feat(zenbrowser): add firefox policies

--- modules/home-manager/applications/web-browser/zen-browser.nix
+++ modules/home-manager/applications/web-browser/zen-browser.nix
@@ -5,5 +5,27 @@
     inputs.zen-browser.homeModules.twilight
   ];
 
-  programs.zen-browser.enable = true;
+  programs.zen-browser = {
+    enable = true;
+
+    # Firefox policies
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
 }
