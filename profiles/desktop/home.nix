9cd75e7d557804c0a7bb1a323d0fc951e4514a09
Mewski <mewski813@gmail.com>
2025-09-12 09:13:24 -0500

    refactor: conventionalize directory names based on domain

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -13,17 +13,17 @@
     ../../modules/home-manager/applications/btop/btop.nix
     ../../modules/home-manager/applications/git/git.nix
     ../../modules/home-manager/applications/neovim/neovim.nix
-    ../../modules/home-manager/applications/terminals/kitty.nix
+    ../../modules/home-manager/applications/terminal/kitty.nix
 
     # Shells
-    ../../modules/home-manager/shells/bash.nix
-    ../../modules/home-manager/shells/fish.nix
+    ../../modules/home-manager/shell/bash.nix
+    ../../modules/home-manager/shell/fish.nix
 
     # Window manager configuration
     ../../modules/home-manager/desktop/hyprland.nix
 
     # Web browser
-    ../../modules/home-manager/applications/web-browsers/zen-browser.nix
+    ../../modules/home-manager/applications/web-browser/zen-browser.nix
   ];
 
   # Install user-specific packages
