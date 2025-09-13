65dfface46c4c0dccca1112410a5ff65fef02872
Mewski <mewski813@gmail.com>
2025-09-13 17:18:20 -0500

    refactor(hyprland): reduce terminal transparency

--- modules/home-manager/theme/stylix.nix
+++ modules/home-manager/theme/stylix.nix
@@ -4,17 +4,8 @@
   # Stylix opacity
   stylix.opacity = {
     applications = 1.0;
-    terminal = 0.75;
+    terminal = 0.8;
     desktop = 1.0;
     popups = 1.0;
   };
-
-  # Enable Stylix theme for kitty
-  stylix.targets.kitty.enable = true;
-
-  # Enable Stylix theme for btop
-  stylix.targets.btop.enable = true;
-
-  # Enable Stylix theme for hyprland
-  stylix.targets.hyprland.enable = true;
 }
