fda17b6de97fb8ab4ce890e347da0a87fb481216
Mewski <mewski813@gmail.com>
2025-09-13 12:39:30 -0500

    fix(stylix): disable buggy fonts

--- themes/catppuccin-mocha/default.nix
+++ themes/catppuccin-mocha/default.nix
@@ -32,18 +32,18 @@
   # stylix.cursor.name = "Bibata-Modern-Ice";
 
   # Fonts
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.override { fonts = [ "JetBrainsMono" ]; };
-      name = "JetBrainsMono Nerd Font Mono";
-    };
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-  };
+  # stylix.fonts = {
+  #   monospace = {
+  #     package = pkgs.nerd-fonts.override { fonts = [ "JetBrainsMono" ]; };
+  #     name = "JetBrainsMono Nerd Font Mono";
+  #   };
+  #   sansSerif = {
+  #     package = pkgs.dejavu_fonts;
+  #     name = "DejaVu Sans";
+  #   };
+  #   serif = {
+  #     package = pkgs.dejavu_fonts;
+  #     name = "DejaVu Serif";
+  #   };
+  # };
 }
