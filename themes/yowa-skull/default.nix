0e23b49f4478ef0dc6615db9972b3971c1c36914
Mewski <mewski813@gmail.com>
2025-09-17 18:06:48 -0500

    feat(stylix): add yowa-skull theme

---
+++ themes/yowa-skull/default.nix
@@ -0,0 +1,29 @@
+{ ... }:
+
+{
+  # Base16 color scheme for yowa-skull theme
+  stylix.base16Scheme = {
+    base00 = "381f21"; # Default background
+    base01 = "594243"; # Lighter background
+    base02 = "796565"; # Selection background
+    base03 = "9a8887"; # Comments, invisibles
+    base04 = "baaba9"; # Dark foreground
+    base05 = "dacecb"; # Default foreground
+    base06 = "e0d5d3"; # Light foreground
+    base07 = "e5dcdb"; # Light background
+    base08 = "805558"; # Variables, XML tags
+    base09 = "ecd2cc"; # Integers, boolean, constants
+    base0A = "e0a096"; # Classes, markup bold
+    base0B = "c7decf"; # Strings, inherited class
+    base0C = "7c9292"; # Support, regular expressions
+    base0D = "305965"; # Functions, methods
+    base0E = "c45860"; # Keywords, storage
+    base0F = "b81d28"; # Deprecated, opening/closing tags
+  };
+
+  # Wallpaper image for the theme
+  stylix.image = ./wallpapers/wall1.png;
+
+  # Color scheme polarity
+  stylix.polarity = "dark";
+}
