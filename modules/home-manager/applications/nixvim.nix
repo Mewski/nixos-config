32757d153ac3468721ec6f6dedf5dceb9f598167
Mewski <mewski813@gmail.com>
2025-09-16 20:18:37 -0500

    fix(nixvim): disable problematic lsp

--- modules/home-manager/applications/nixvim.nix
+++ modules/home-manager/applications/nixvim.nix
@@ -6,5 +6,16 @@
   ];
 
   # Neovim configuration framework with Nix integration
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+
+    # Disable problematic language servers
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        # Disable dockerfile language server
+        dockerls.enable = false;
+      };
+    };
+  };
 }
