727a3caea053ca0100c7147d1604fd2462038517
Mewski <mewski813@gmail.com>
2025-09-17 21:14:21 -0500

    refactor(stylix): update yowa-skull color scheme

--- themes/yowa-skull/default.nix
+++ themes/yowa-skull/default.nix
@@ -3,14 +3,14 @@
 {
   # Base16 color scheme for yowa-skull theme
   stylix.base16Scheme = {
-    base00 = "381f21"; # Default background
-    base01 = "594243"; # Lighter background
-    base02 = "796565"; # Selection background
-    base03 = "9a8887"; # Comments, invisibles
-    base04 = "baaba9"; # Dark foreground
-    base05 = "dacecb"; # Default foreground
-    base06 = "e0d5d3"; # Light foreground
-    base07 = "e5dcdb"; # Light background
+    base00 = "040604"; # Default background
+    base01 = "2d2e2f"; # Lighter background
+    base02 = "57575b"; # Selection background
+    base03 = "818087"; # Comments, invisibles
+    base04 = "aba9b3"; # Dark foreground
+    base05 = "d5d2df"; # Default foreground
+    base06 = "dbd9e4"; # Light foreground
+    base07 = "e1e0e8"; # Light background
     base08 = "805558"; # Variables, XML tags
     base09 = "ecd2cc"; # Integers, boolean, constants
     base0A = "e0a096"; # Classes, markup bold
