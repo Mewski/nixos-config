ab620abfe7bc3d5fd3acc56363d67f28e2e5eb6f
Mewski <mewski813@gmail.com>
2025-09-17 21:19:34 -0500

    refactor(stylix): change yowa-skull color scheme to everforest

--- themes/yowa-skull/default.nix
+++ themes/yowa-skull/default.nix
@@ -3,22 +3,22 @@
 {
   # Base16 color scheme for yowa-skull theme
   stylix.base16Scheme = {
-    base00 = "040604"; # Default background
-    base01 = "2d2e2f"; # Lighter background
-    base02 = "57575b"; # Selection background
-    base03 = "818087"; # Comments, invisibles
-    base04 = "aba9b3"; # Dark foreground
-    base05 = "d5d2df"; # Default foreground
-    base06 = "dbd9e4"; # Light foreground
-    base07 = "e1e0e8"; # Light background
-    base08 = "805558"; # Variables, XML tags
-    base09 = "ecd2cc"; # Integers, boolean, constants
-    base0A = "e0a096"; # Classes, markup bold
-    base0B = "c7decf"; # Strings, inherited class
-    base0C = "7c9292"; # Support, regular expressions
-    base0D = "305965"; # Functions, methods
-    base0E = "c45860"; # Keywords, storage
-    base0F = "b81d28"; # Deprecated, opening/closing tags
+    base00 = "272e33"; # Default background
+    base01 = "2e383c"; # Lighter background
+    base02 = "414b50"; # Selection background
+    base03 = "859289"; # Comments, invisibles
+    base04 = "9da9a0"; # Dark foreground
+    base05 = "d3c6aa"; # Default foreground
+    base06 = "e4e1cd"; # Light foreground
+    base07 = "fdf6e3"; # Light background
+    base08 = "7fbbb3"; # Variables, XML tags
+    base09 = "d699b6"; # Integers, boolean, constants
+    base0A = "dbbc7f"; # Classes, markup bold
+    base0B = "83c092"; # Strings, inherited class
+    base0C = "e69875"; # Support, regular expressions
+    base0D = "a7c080"; # Functions, methods
+    base0E = "e67e80"; # Keywords, storage
+    base0F = "4C3743"; # Deprecated, opening/closing tags
   };
 
   # Wallpaper image for the theme
