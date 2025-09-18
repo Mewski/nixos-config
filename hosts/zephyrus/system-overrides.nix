2d67652fa09edcae57f4a803aa586ba4ff651d14
Mewski <mewski813@gmail.com>
2025-09-18 00:35:10 -0500

    fix(asusd): use correct option for asusd config

--- hosts/zephyrus/system-overrides.nix
+++ hosts/zephyrus/system-overrides.nix
@@ -1,47 +1,50 @@
 { ... }:
 
 {
-  services.asusd.profileConfig.text = ''
-    (
-      charge_control_end_threshold: 80,
-      disable_nvidia_powerd_on_battery: true,
-      ac_command: "",
-      bat_command: "",
-      platform_profile_linked_epp: true,
-      platform_profile_on_battery: Quiet,
-      change_platform_profile_on_battery: true,
-      platform_profile_on_ac: Balanced,
-      change_platform_profile_on_ac: true,
-      profile_quiet_epp: Power,
-      profile_balanced_epp: BalancePower,
-      profile_custom_epp: Performance,
-      profile_performance_epp: Performance,
-      ac_profile_tunings: {
-          Performance: (
-              enabled: false,
-              group: {},
-          ),
-          Quiet: (
-              enabled: false,
-              group: {},
-          ),
-          Balanced: (
-              enabled: false,
-              group: {},
-          ),
-      },
-      dc_profile_tunings: {
-          Quiet: (
-              enabled: false,
-              group: {},
-          ),
-          Balanced: (
-              enabled: false,
-              group: {},
-          ),
-          Performance: (
-              enabled: false,
-              group: {},
+  services.asusd = {
+    enable = true;
+
+    asusdConfig.text = ''
+      (
+        charge_control_end_threshold: 80,
+        disable_nvidia_powerd_on_battery: true,
+        ac_command: "",
+        bat_command: "",
+        platform_profile_linked_epp: true,
+        platform_profile_on_battery: Quiet,
+        change_platform_profile_on_battery: true,
+        platform_profile_on_ac: Balanced,
+        change_platform_profile_on_ac: true,
+        profile_quiet_epp: Power,
+        profile_balanced_epp: BalancePower,
+        profile_custom_epp: Performance,
+        profile_performance_epp: Performance,
+        ac_profile_tunings: {
+            Performance: (
+                enabled: false,
+                group: {},
+            ),
+            Quiet: (
+                enabled: false,
+                group: {},
+            ),
+            Balanced: (
+                enabled: false,
+                group: {},
+            ),
+        },
+        dc_profile_tunings: {
+            Quiet: (
+                enabled: false,
+                group: {},
+            ),
+            Balanced: (
+                enabled: false,
+                group: {},
+            ),
+            Performance: (
+                enabled: false,
+                group: {},
           ),
       },
       armoury_settings: {},
