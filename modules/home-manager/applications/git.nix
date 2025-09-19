47940440fb10ea145fb4c092a8cbe6a249190e5e
Mewski <mewski813@gmail.com>
2025-09-19 00:14:09 -0500

    feat(git): add configuration

--- modules/home-manager/applications/git.nix
+++ modules/home-manager/applications/git.nix
@@ -1,6 +1,51 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
-  # Distributed version control system
-  programs.git.enable = true;
+  # Distributed version control system with enhanced configuration
+  programs.git = {
+    enable = true;
+
+    # Git Large File Storage for handling large binary files
+    lfs = {
+      enable = true;
+      package = pkgs.git-lfs;
+    };
+
+    # User identity configuration
+    userEmail = "mewski@mewski.dev";
+    userName = "Mewski";
+
+    # Core Git configuration
+    extraConfig = {
+      # Commit signing configuration
+      commit = {
+        gpgsign = true;
+      };
+
+      # Tag signing configuration
+      tag = {
+        forcesignannotated = true;
+      };
+
+      # GPG configuration for SSH signing
+      gpg = {
+        format = "ssh";
+      };
+
+      # User signing key configuration
+      user = {
+        signingkey = "${config.home.homeDirectory}/.ssh/id_25519";
+      };
+
+      # Core editor configuration
+      core = {
+        editor = "nvim";
+      };
+
+      # Default branch naming
+      init = {
+        defaultBranch = "master";
+      };
+    };
+  };
 }
