f36a845c75779312368ffd7ad230cbb85ec95eda
Mewski <mewski813@gmail.com>
2025-10-28 01:45:25 -0500

    fix(hyprland): move window rules into settings

--- modules/desktops/hyprland/layout.nix
+++ modules/desktops/hyprland/layout.nix
@@ -9,12 +9,12 @@
       general.layout = "dwindle";
 
       master.new_status = "master";
+
+      windowrule = [
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+
+        "suppressevent maximize, class:.*"
+      ];
     };
-
-    windowrule = [
-      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-
-      "suppressevent maximize, class:.*"
-    ];
   };
 }
