95fa2618d005922057595b9da0dabd4bd487af51
Mewski <mewski813@gmail.com>
2025-11-19 00:43:21 -0600

    chore: update key configuration

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,33 +1,37 @@
 {
-  flake.homeModules.git = {
-    programs.git = {
-      enable = true;
+  flake.homeModules.git =
+    { config, ... }:
+    {
+      programs.git = {
+        enable = true;
 
-      userName = "Mewski";
-      userEmail = "mewski813@gmail.com";
+        userName = "Mewski";
+        userEmail = "mewski813@gmail.com";
 
-      signing = {
-        key = "<key>";
-        signByDefault = true;
+        signing = {
+          key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+          signByDefault = true;
+        };
+
+        extraConfig.gpg.format = "ssh";
       };
-    };
 
-    programs.gh = {
-      enable = true;
+      programs.gh = {
+        enable = true;
 
-      hosts = {
-        "github.com" = {
-          user = "Mewski";
+        hosts = {
+          "github.com" = {
+            user = "Mewski";
+          };
+        };
+
+        gitCredentialHelper = {
+          enable = true;
         };
       };
 
-      gitCredentialHelper = {
-        enable = true;
-      };
+      persist.directories = [
+        ".config/gh"
+      ];
     };
-
-    persist.directories = [
-      ".config/gh"
-    ];
-  };
 }
