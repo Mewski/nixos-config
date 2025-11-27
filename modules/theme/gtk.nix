675ea254ff8ffc0a6773aeb3fc0d96aa5de5ada0
Mewski <mewski813@gmail.com>
2025-11-27 00:51:37 -0600

    feat: add gtk theme

--- modules/theme/gtk.nix
+++ modules/theme/gtk.nix
@@ -1,3 +1,121 @@
 {
-  flake.homeModules.theme = { };
+  flake.homeModules.theme =
+    {
+      pkgs,
+      scheme,
+      fonts,
+      polarity,
+      ...
+    }:
+    {
+      gtk = {
+        enable = true;
+
+        theme = {
+          package = pkgs.adw-gtk3;
+          name = if polarity == "dark" then "adw-gtk3-dark" else "adw-gtk3";
+        };
+
+        font = {
+          name = fonts.sansSerif.name;
+          size = fonts.sizes.desktop;
+        };
+
+        gtk3.extraCss = ''
+          @define-color accent_color ${scheme.withHashtag.base0D};
+          @define-color accent_bg_color ${scheme.withHashtag.base0D};
+          @define-color accent_fg_color ${scheme.withHashtag.base00};
+          @define-color destructive_color ${scheme.withHashtag.base08};
+          @define-color destructive_bg_color ${scheme.withHashtag.base08};
+          @define-color destructive_fg_color ${scheme.withHashtag.base00};
+          @define-color success_color ${scheme.withHashtag.base0B};
+          @define-color success_bg_color ${scheme.withHashtag.base0B};
+          @define-color success_fg_color ${scheme.withHashtag.base00};
+          @define-color warning_color ${scheme.withHashtag.base0A};
+          @define-color warning_bg_color ${scheme.withHashtag.base0A};
+          @define-color warning_fg_color ${scheme.withHashtag.base00};
+          @define-color error_color ${scheme.withHashtag.base08};
+          @define-color error_bg_color ${scheme.withHashtag.base08};
+          @define-color error_fg_color ${scheme.withHashtag.base00};
+          @define-color window_bg_color ${scheme.withHashtag.base00};
+          @define-color window_fg_color ${scheme.withHashtag.base05};
+          @define-color view_bg_color ${scheme.withHashtag.base00};
+          @define-color view_fg_color ${scheme.withHashtag.base05};
+          @define-color headerbar_bg_color ${scheme.withHashtag.base01};
+          @define-color headerbar_fg_color ${scheme.withHashtag.base05};
+          @define-color headerbar_border_color ${scheme.withHashtag.base00};
+          @define-color headerbar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color headerbar_shade_color rgba(0, 0, 0, 0.36);
+          @define-color card_bg_color ${scheme.withHashtag.base01};
+          @define-color card_fg_color ${scheme.withHashtag.base05};
+          @define-color card_shade_color rgba(0, 0, 0, 0.36);
+          @define-color dialog_bg_color ${scheme.withHashtag.base01};
+          @define-color dialog_fg_color ${scheme.withHashtag.base05};
+          @define-color popover_bg_color ${scheme.withHashtag.base01};
+          @define-color popover_fg_color ${scheme.withHashtag.base05};
+          @define-color shade_color rgba(0, 0, 0, 0.36);
+          @define-color scrollbar_outline_color ${scheme.withHashtag.base00};
+          @define-color sidebar_bg_color ${scheme.withHashtag.base00};
+          @define-color sidebar_fg_color ${scheme.withHashtag.base05};
+          @define-color sidebar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color sidebar_shade_color rgba(0, 0, 0, 0.36);
+          @define-color secondary_sidebar_bg_color ${scheme.withHashtag.base00};
+          @define-color secondary_sidebar_fg_color ${scheme.withHashtag.base05};
+          @define-color secondary_sidebar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color secondary_sidebar_shade_color rgba(0, 0, 0, 0.36);
+        '';
+
+        gtk4.extraCss = ''
+          @define-color accent_color ${scheme.withHashtag.base0D};
+          @define-color accent_bg_color ${scheme.withHashtag.base0D};
+          @define-color accent_fg_color ${scheme.withHashtag.base00};
+          @define-color destructive_color ${scheme.withHashtag.base08};
+          @define-color destructive_bg_color ${scheme.withHashtag.base08};
+          @define-color destructive_fg_color ${scheme.withHashtag.base00};
+          @define-color success_color ${scheme.withHashtag.base0B};
+          @define-color success_bg_color ${scheme.withHashtag.base0B};
+          @define-color success_fg_color ${scheme.withHashtag.base00};
+          @define-color warning_color ${scheme.withHashtag.base0A};
+          @define-color warning_bg_color ${scheme.withHashtag.base0A};
+          @define-color warning_fg_color ${scheme.withHashtag.base00};
+          @define-color error_color ${scheme.withHashtag.base08};
+          @define-color error_bg_color ${scheme.withHashtag.base08};
+          @define-color error_fg_color ${scheme.withHashtag.base00};
+          @define-color window_bg_color ${scheme.withHashtag.base00};
+          @define-color window_fg_color ${scheme.withHashtag.base05};
+          @define-color view_bg_color ${scheme.withHashtag.base00};
+          @define-color view_fg_color ${scheme.withHashtag.base05};
+          @define-color headerbar_bg_color ${scheme.withHashtag.base01};
+          @define-color headerbar_fg_color ${scheme.withHashtag.base05};
+          @define-color headerbar_border_color ${scheme.withHashtag.base00};
+          @define-color headerbar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color headerbar_shade_color rgba(0, 0, 0, 0.36);
+          @define-color card_bg_color ${scheme.withHashtag.base01};
+          @define-color card_fg_color ${scheme.withHashtag.base05};
+          @define-color card_shade_color rgba(0, 0, 0, 0.36);
+          @define-color dialog_bg_color ${scheme.withHashtag.base01};
+          @define-color dialog_fg_color ${scheme.withHashtag.base05};
+          @define-color popover_bg_color ${scheme.withHashtag.base01};
+          @define-color popover_fg_color ${scheme.withHashtag.base05};
+          @define-color shade_color rgba(0, 0, 0, 0.36);
+          @define-color scrollbar_outline_color ${scheme.withHashtag.base00};
+          @define-color sidebar_bg_color ${scheme.withHashtag.base00};
+          @define-color sidebar_fg_color ${scheme.withHashtag.base05};
+          @define-color sidebar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color sidebar_shade_color rgba(0, 0, 0, 0.36);
+          @define-color secondary_sidebar_bg_color ${scheme.withHashtag.base00};
+          @define-color secondary_sidebar_fg_color ${scheme.withHashtag.base05};
+          @define-color secondary_sidebar_backdrop_color ${scheme.withHashtag.base01};
+          @define-color secondary_sidebar_shade_color rgba(0, 0, 0, 0.36);
+        '';
+
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = polarity == "dark";
+        };
+
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = polarity == "dark";
+        };
+      };
+    };
 }
