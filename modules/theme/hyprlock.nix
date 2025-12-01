e425f6109bcb85ac12f5e582b82557da98c7840e
Mewski <mewski813@gmail.com>
2025-11-30 21:05:29 -0600

    fix(hyprlock): remove fade on empty

--- modules/theme/hyprlock.nix
+++ modules/theme/hyprlock.nix
@@ -12,7 +12,28 @@
           font_color = "rgb(${scheme.base05})";
           fail_color = "rgb(${scheme.base08})";
           check_color = "rgb(${scheme.base0A})";
+          fade_on_empty = false;
+          placeholder_text = ''<span font_style="normal">Input Password</span>'';
+          fail_text = ''<span font_style="normal">Authentication Failed</span>'';
         };
+        label = [
+          {
+            text = ''cmd[update:1000] date +"%H:%M:%S"'';
+            color = "rgb(${scheme.base05})";
+            font_size = 96;
+            position = "0, -100";
+            halign = "center";
+            valign = "top";
+          }
+          {
+            text = ''cmd[update:1000] date +"%A, %B %d"'';
+            color = "rgb(${scheme.base04})";
+            font_size = 24;
+            position = "0, -250";
+            halign = "center";
+            valign = "top";
+          }
+        ];
       };
     };
 }
