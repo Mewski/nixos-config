fc5d2348d552fe308571023004412e1db5602073
Mewski <mewski813@gmail.com>
2025-12-16 13:08:07 -0600

    feat(obsidian): add community plugins

--- modules/programs/obsidian.nix
+++ modules/programs/obsidian.nix
@@ -1,14 +1,65 @@
 {
-  flake.homeModules.obsidian = {
-    programs.obsidian = {
-      enable = true;
+  flake.homeModules.obsidian =
+    { pkgs, ... }:
+    let
+      buildObsidianPlugin =
+        {
+          pname,
+          version,
+          owner,
+          repo,
+          rev ? version,
+          sha256,
+        }:
+        pkgs.stdenv.mkDerivation {
+          inherit pname version;
 
-      vaults = {
-        notes = {
-          enable = true;
-          target = "Documents/Notes";
+          src = pkgs.fetchFromGitHub {
+            inherit
+              owner
+              repo
+              rev
+              sha256
+              ;
+          };
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r * $out/
+          '';
+        };
+
+      excalidraw = buildObsidianPlugin {
+        pname = "obsidian-excalidraw-plugin";
+        version = "2.18.0";
+        owner = "zsviczian";
+        repo = "obsidian-excalidraw-plugin";
+        sha256 = "sha256-4f8lUin3Cvo+7+Y06b0F7PcTbSOAAAGR5FYXqfLyVqA=";
+      };
+
+      latex = buildObsidianPlugin {
+        pname = "obsidian-latex-suite";
+        version = "1.9.8";
+        owner = "artisticat1";
+        repo = "obsidian-latex-suite";
+        sha256 = "sha256-TtshzrHDnXG8xSGQvEmCrFLaEM8ckVrZLG1q6RBwHvo=";
+      };
+    in
+    {
+      programs.obsidian = {
+        enable = true;
+
+        defaultSettings.communityPlugins = [
+          excalidraw
+          latex
+        ];
+
+        vaults = {
+          notes = {
+            enable = true;
+            target = "Documents/Notes";
+          };
         };
       };
     };
-  };
 }
