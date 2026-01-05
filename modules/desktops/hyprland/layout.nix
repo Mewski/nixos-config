0a7f3da575b20c41d52481fc6f2add192f579a95
Mewski <mewski@mewski.dev>
2026-01-04 20:06:57 -0600

    fix: correct xwayland focus rule

--- modules/desktops/hyprland/layout.nix
+++ modules/desktops/hyprland/layout.nix
@@ -13,6 +13,7 @@
       master.new_status = "master";
 
       windowrule = [
+        "no_initial_focus on, match:class ^$, match:title ^$, match:xwayland true, match:float true, match:fullscreen false, match:pin false"
         "suppress_event maximize, match:class .*"
         "immediate on, match:xwayland 1"
       ];
