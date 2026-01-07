66b3185e8f0f39beaad141eb026ea3b183e7645a
Mewski <mewski@mewski.dev>
2026-01-07 10:59:59 -0600

    fix(gitlab): use cloudflare connecting ip

--- modules/hosts/astraeus/gitlab.nix
+++ modules/hosts/astraeus/gitlab.nix
@@ -20,6 +20,31 @@
         ];
       };
 
+      cloudflareIPs = [
+        "173.245.48.0/20"
+        "103.21.244.0/22"
+        "103.22.200.0/22"
+        "103.31.4.0/22"
+        "141.101.64.0/18"
+        "108.162.192.0/18"
+        "190.93.240.0/20"
+        "188.114.96.0/20"
+        "197.234.240.0/22"
+        "198.41.128.0/17"
+        "162.158.0.0/15"
+        "104.16.0.0/13"
+        "104.24.0.0/14"
+        "172.64.0.0/13"
+        "131.0.72.0/22"
+        "2400:cb00::/32"
+        "2606:4700::/32"
+        "2803:f800::/32"
+        "2405:b500::/32"
+        "2405:8100::/32"
+        "2a06:98c0::/29"
+        "2c0f:f248::/32"
+      ];
+
       proxyHeaders = ''
         proxy_set_header X-Forwarded-Proto https;
         proxy_set_header X-Forwarded-Ssl on;
@@ -100,6 +125,11 @@
           gitlab_kas.enabled = false;
         };
       };
+
+      services.nginx.commonHttpConfig = ''
+        ${builtins.concatStringsSep "\n" (map (ip: "set_real_ip_from ${ip};") cloudflareIPs)}
+        real_ip_header CF-Connecting-IP;
+      '';
 
       services.nginx.virtualHosts = {
         ${domain} = sslConfig // {
