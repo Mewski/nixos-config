eb4d67fd4d16250c9e75aa5011f63def860223d6
Mewski <mewski@mewski.dev>
2026-01-31 04:22:15 -0600

    fix(astraeus): update routes

--- modules/hosts/astraeus/wireguard.nix
+++ modules/hosts/astraeus/wireguard.nix
@@ -14,6 +14,7 @@
         postSetup = ''
           ip rule add to 10.0.0.0/8 lookup main priority 100
           ip rule add to 192.168.0.0/16 lookup main priority 100
+          ip rule add to 144.202.48.125 lookup main priority 100
           ip rule add from 23.152.236.0/28 lookup 100 priority 200
           ip rule add lookup 100 priority 300
           ip route add default dev wg-aeolus src 23.152.236.2 table 100
@@ -27,6 +28,7 @@
         postShutdown = ''
           ip rule del to 10.0.0.0/8 lookup main priority 100 || true
           ip rule del to 192.168.0.0/16 lookup main priority 100 || true
+          ip rule del to 144.202.48.125 lookup main priority 100 || true
           ip rule del from 23.152.236.0/28 lookup 100 priority 200 || true
           ip rule del lookup 100 priority 300 || true
           ip route del default dev wg-aeolus src 23.152.236.2 table 100 || true
