cb79c6230e099ba5074b94329e68532aac53ae40
Mewski <mewski@mewski.dev>
2026-01-31 03:47:02 -0600

    fix(astraeus): update setup rules

--- modules/hosts/astraeus/wireguard.nix
+++ modules/hosts/astraeus/wireguard.nix
@@ -9,6 +9,20 @@
         ];
         privateKeyFile = config.sops.secrets."wireguard/private_key".path;
 
+        postSetup = ''
+          ip rule add from 23.152.236.0/28 table 100
+          ip route add default dev wg-aeolus table 100
+          ip -6 rule add from 2602:fe18::/60 table 100
+          ip -6 route add default dev wg-aeolus table 100
+        '';
+
+        postShutdown = ''
+          ip rule del from 23.152.236.0/28 table 100 || true
+          ip route del default dev wg-aeolus table 100 || true
+          ip -6 rule del from 2602:fe18::/60 table 100 || true
+          ip -6 route del default dev wg-aeolus table 100 || true
+        '';
+
         peers = [
           {
             publicKey = "Ez3Fx1SmQHoPEuGasq235KNMPr3TqK6CCPE+VTnwUCk=";
