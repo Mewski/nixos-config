a97bed3eea786c730636f904f3cfb855137fb6b0
Mewski <mewski@mewski.dev>
2026-02-13 09:33:33 -0600

    fix: give docker privilege

--- modules/hosts/prometheus/gitlab-runner.nix
+++ modules/hosts/prometheus/gitlab-runner.nix
@@ -8,6 +8,7 @@
         services.docker-runner = {
           authenticationTokenConfigFile = config.sops.secrets."gitlab-runner/token".path;
           dockerImage = "alpine:latest";
+          dockerPrivileged = true;
           executor = "docker";
         };
       };
