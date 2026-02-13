466107ca13958e8c714629d65ca5f366f835585f
Mewski <mewski@mewski.dev>
2026-02-13 09:55:19 -0600

    chore: add certs to docker volume

--- modules/hosts/prometheus/gitlab-runner.nix
+++ modules/hosts/prometheus/gitlab-runner.nix
@@ -9,6 +9,15 @@
           authenticationTokenConfigFile = config.sops.secrets."gitlab-runner/token".path;
           dockerImage = "alpine:latest";
           dockerPrivileged = true;
+          dockerVolumes = [
+            "/certs/client"
+          ];
+          environmentVariables = {
+            DOCKER_TLS_CERTDIR = "/certs";
+            DOCKER_CERT_PATH = "/certs/client";
+            DOCKER_HOST = "tcp://docker:2376";
+            DOCKER_TLS_VERIFY = "1";
+          };
           executor = "docker";
         };
       };
