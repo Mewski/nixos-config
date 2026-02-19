65989b0ea93ed3e01a15d697439eefc62351643a
Mewski <mewski@mewski.dev>
2026-02-19 03:40:14 -0600

    chore: update ares

--- modules/hosts/ares/wireguard.nix
+++ modules/hosts/ares/wireguard.nix
@@ -21,17 +21,21 @@
 
         postSetup = ''
           ${ip} rule add from 23.152.236.16/28 lookup 100
+          ${ip} route add 23.152.236.16/28 dev vmbr1 table 100
           ${ip} route add default dev wg0 table 100
 
           ${ip} -6 rule add from 2602:fe18:1::/48 lookup 100
+          ${ip} -6 route add 2602:fe18:1::/48 dev vmbr1 table 100
           ${ip} -6 route add default dev wg0 table 100
         '';
 
         postShutdown = ''
           ${ip} rule del from 23.152.236.16/28 lookup 100 || true
+          ${ip} route del 23.152.236.16/28 dev vmbr1 table 100 || true
           ${ip} route del default dev wg0 table 100 || true
 
           ${ip} -6 rule del from 2602:fe18:1::/48 lookup 100 || true
+          ${ip} -6 route del 2602:fe18:1::/48 dev vmbr1 table 100 || true
           ${ip} -6 route del default dev wg0 table 100 || true
         '';
 
