b016c4be5fbe62305e569fa1314a8390f69f8b0b
Mewski <mewski@mewski.dev>
2026-02-22 18:49:37 -0600

    chore: update binary ninja

--- modules/programs/binary-ninja.nix
+++ modules/programs/binary-ninja.nix
@@ -1,16 +1,10 @@
-{ inputs, ... }:
 {
   flake.homeModules.binary-ninja =
     { pkgs, ... }:
     {
-      imports = [ inputs.binary-ninja.hmModules.binaryninja ];
-
-      programs.binary-ninja = {
-        enable = true;
-        package = pkgs.binary-ninja-personal-wayland;
-      };
-
       home = {
+        packages = [ pkgs.binary-ninja-personal-wayland ];
+
         file.".binaryninja/settings.json".text = builtins.toJSON {
           "ui.theme.name" = "Ninja Dark";
           "network.enableUpdates" = false;
