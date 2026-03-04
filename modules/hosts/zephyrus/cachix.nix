94427f7ae182976a990f83e41c2ec50242930eb6
Mewski <mewski@mewski.dev>
2026-03-03 22:32:53 -0600

    chore: remove zed cachix

--- modules/hosts/zephyrus/cachix.nix
+++ modules/hosts/zephyrus/cachix.nix
@@ -5,13 +5,11 @@
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://cache.nixos-cuda.org"
-        "https://zed-industries.cachix.org"
       ];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-        "zed-industries.cachix.org-1:fgVpvtdF+ssrgP1lB6EusuR3uM6bNcncWduKxri3u6Y="
       ];
     };
   };
