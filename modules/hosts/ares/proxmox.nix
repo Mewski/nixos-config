8bdf2db7b0afdc95cb6a379b52f08ab26ab39f90
Mewski <mewski@mewski.dev>
2026-03-07 17:21:14 -0600

    chore: fix openssl

--- modules/hosts/ares/proxmox.nix
+++ modules/hosts/ares/proxmox.nix
@@ -1,44 +1,45 @@
 { inputs, ... }:
 {
-  flake.nixosModules.ares = { pkgs, lib, ... }: {
-    imports = [ inputs.proxmox-nixos.nixosModules.proxmox-ve ];
+  flake.nixosModules.ares =
+    { pkgs, lib, ... }:
+    {
+      imports = [ inputs.proxmox-nixos.nixosModules.proxmox-ve ];
 
-    nixpkgs.overlays = [ inputs.proxmox-nixos.overlays.x86_64-linux ];
+      nixpkgs.overlays = [ inputs.proxmox-nixos.overlays.x86_64-linux ];
 
-    environment.etc."ssl/openssl-override.cnf".source =
-      let
-        opensslDir = "${pkgs.openssl}/etc/ssl";
-      in
+      environment.etc."ssl/openssl-override.cnf".source =
+        let
+          opensslDir = "${pkgs.openssl}/etc/ssl";
+        in
         pkgs.runCommand "openssl-cnf-patched" { } ''
           sed 's/# activate = 1/activate = 1/' ${opensslDir}/openssl.cnf > $out
         '';
 
-    systemd.services.pveproxy.serviceConfig.ExecStartPre = lib.mkBefore [
-      "+${pkgs.bash}/bin/bash -c '${pkgs.util-linux}/bin/mount --bind /etc/ssl/openssl-override.cnf ${pkgs.openssl}/etc/ssl/openssl.cnf'"
-    ];
-
-    boot.kernel.sysctl = {
-      "net.bridge.bridge-nf-call-iptables" = 0;
-      "net.bridge.bridge-nf-call-ip6tables" = 0;
-    };
-
-    networking.bridges = {
-      vmbr0.interfaces = [ "ens1f0" ];
-      vmbr1.interfaces = [ ];
-      vmbr2.interfaces = [ ];
-    };
-
-    networking.firewall.allowedTCPPorts = [ 8006 ];
-
-    services.proxmox-ve = {
-      enable = true;
-      ipAddress = "10.0.50.10";
-      bridges = [
-        "vmbr0"
-        "vmbr1"
-        "vmbr2"
+      systemd.services.pveproxy.serviceConfig.ExecStartPre = lib.mkBefore [
+        "+${pkgs.bash}/bin/bash -c '${pkgs.util-linux}/bin/mount --bind /etc/ssl/openssl-override.cnf ${pkgs.openssl}/etc/ssl/openssl.cnf'"
       ];
+
+      boot.kernel.sysctl = {
+        "net.bridge.bridge-nf-call-iptables" = 0;
+        "net.bridge.bridge-nf-call-ip6tables" = 0;
+      };
+
+      networking.bridges = {
+        vmbr0.interfaces = [ "ens1f0" ];
+        vmbr1.interfaces = [ ];
+        vmbr2.interfaces = [ ];
+      };
+
+      networking.firewall.allowedTCPPorts = [ 8006 ];
+
+      services.proxmox-ve = {
+        enable = true;
+        ipAddress = "10.0.50.10";
+        bridges = [
+          "vmbr0"
+          "vmbr1"
+          "vmbr2"
+        ];
+      };
     };
-  };
-  };
 }
