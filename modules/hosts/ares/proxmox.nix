13b03ab834ed1a6d1712d9a7b40ace5c311eb7d7
Mewski <mewski@mewski.dev>
2026-03-07 20:53:18 -0600

    chore: update tpm

--- modules/hosts/ares/proxmox.nix
+++ modules/hosts/ares/proxmox.nix
@@ -1,42 +1,36 @@
 { inputs, ... }:
 {
-  flake.nixosModules.ares =
-    { pkgs, lib, ... }:
-    {
-      imports = [ inputs.proxmox-nixos.nixosModules.proxmox-ve ];
+  flake.nixosModules.ares = {
+    imports = [ inputs.proxmox-nixos.nixosModules.proxmox-ve ];
 
-      nixpkgs.overlays = [ inputs.proxmox-nixos.overlays.x86_64-linux ];
+    nixpkgs.overlays = [
+      inputs.proxmox-nixos.overlays.x86_64-linux
+      (_final: prev: {
+        pve-qemu = prev.pve-qemu.override { fuseSupport = true; };
+      })
+    ];
 
-      environment.etc."ssl/openssl-override.cnf".source =
-        pkgs.runCommand "openssl-cnf-patched" { } ''
-          sed 's/# activate = 1/activate = 1/' ${pkgs.openssl.out}/etc/ssl/openssl.cnf > $out
-        '';
-
-      systemd.services.pveproxy.serviceConfig.ExecStartPre = lib.mkBefore [
-        "+${pkgs.bash}/bin/bash -c '${pkgs.util-linux}/bin/mount --bind /etc/ssl/openssl-override.cnf ${pkgs.openssl.out}/etc/ssl/openssl.cnf'"
-      ];
-
-      boot.kernel.sysctl = {
-        "net.bridge.bridge-nf-call-iptables" = 0;
-        "net.bridge.bridge-nf-call-ip6tables" = 0;
-      };
-
-      networking.bridges = {
-        vmbr0.interfaces = [ "ens1f0" ];
-        vmbr1.interfaces = [ ];
-        vmbr2.interfaces = [ ];
-      };
-
-      networking.firewall.allowedTCPPorts = [ 8006 ];
-
-      services.proxmox-ve = {
-        enable = true;
-        ipAddress = "10.0.50.10";
-        bridges = [
-          "vmbr0"
-          "vmbr1"
-          "vmbr2"
-        ];
-      };
+    boot.kernel.sysctl = {
+      "net.bridge.bridge-nf-call-iptables" = 0;
+      "net.bridge.bridge-nf-call-ip6tables" = 0;
     };
+
+    networking.bridges = {
+      vmbr0.interfaces = [ "ens1f0" ];
+      vmbr1.interfaces = [ ];
+      vmbr2.interfaces = [ ];
+    };
+
+    networking.firewall.allowedTCPPorts = [ 8006 ];
+
+    services.proxmox-ve = {
+      enable = true;
+      ipAddress = "10.0.50.10";
+      bridges = [
+        "vmbr0"
+        "vmbr1"
+        "vmbr2"
+      ];
+    };
+  };
 }
