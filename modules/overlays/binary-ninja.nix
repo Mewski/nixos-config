26295640a68094692613a7b11def78b950ad00e3
Mewski <mewski@mewski.dev>
2026-03-14 23:06:19 -0500

    chore: fix binary ninja version

--- modules/overlays/binary-ninja.nix
+++ modules/overlays/binary-ninja.nix
@@ -14,6 +14,15 @@
               sha256 = "7afd705f5cd65da4bf0c5f0bc6c073717f873b268d23bd0fbf2797a21f7d4a81";
             };
 
+            postFixup = (old.postFixup or "") + ''
+              rm $out/bin/binaryninja
+              makeWrapper $out/opt/binaryninja/binaryninja $out/bin/binaryninja \
+                --prefix PYTHONPATH : "$program_PYTHONPATH" \
+                --set QT_QPA_PLATFORM wayland \
+                --set QT_PLUGIN_PATH "$out/opt/binaryninja/qt" \
+                --unset QT_STYLE_OVERRIDE
+            '';
+
             postInstall = (old.postInstall or "") + ''
               cat >> $out/opt/binaryninja/python/binaryninja/_binaryninjacore.py << 'EOF'
 
