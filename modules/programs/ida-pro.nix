f06776099974743aaefec6319bd0b59e3ebeb8a6
Mewski <mewski@mewski.dev>
2026-04-05 00:05:17 -0500

    feat: update plugins

--- modules/programs/ida-pro.nix
+++ modules/programs/ida-pro.nix
@@ -1,12 +1,23 @@
 {
   flake.homeModules.ida-pro =
     { lib, pkgs, ... }:
+    let
+      vulfi = pkgs.fetchFromGitHub {
+        owner = "Accenture";
+        repo = "VulFi";
+        rev = "0bb7fdf8ccb906600cc209c35daf05774172acc8";
+        hash = "sha256-P2bfFe3LgwtGxajaG7MPcCHCIg9P1YyaRMogR9KBkBU=";
+      };
+    in
     {
       home = {
         packages = [
           pkgs.ida-pro-wayland
           pkgs.ida-pro-mcp
         ];
+
+        file.".idapro/plugins/vulfi.py".source = "${vulfi}/vulfi.py";
+        file.".idapro/plugins/vulfi_prototypes.json".source = "${vulfi}/vulfi_prototypes.json";
 
         activation.ida-pro-mcp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           ${pkgs.ida-pro-mcp}/bin/ida-pro-mcp --install 2>/dev/null || true
