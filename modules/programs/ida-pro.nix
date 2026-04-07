5e2bbfd17fa80e45f4b0c09762b1ae7bf9388832
Mewski <mewski@mewski.dev>
2026-04-06 23:35:31 -0500

    feat: add ida pro wakatime

--- modules/programs/ida-pro.nix
+++ modules/programs/ida-pro.nix
@@ -8,6 +8,12 @@
         rev = "0bb7fdf8ccb906600cc209c35daf05774172acc8";
         hash = "sha256-P2bfFe3LgwtGxajaG7MPcCHCIg9P1YyaRMogR9KBkBU=";
       };
+      ida-wakatime = pkgs.fetchFromGitHub {
+        owner = "es3n1n";
+        repo = "ida-wakatime-py";
+        rev = "68a13d1fbf13f901c735b266715ac5e467517270";
+        hash = "sha256-a4prB6UURs2O187TlPB5wLwa4pxvnNoLB2u+aI5Y8Bw=";
+      };
     in
     {
       home = {
@@ -18,6 +24,7 @@
 
         file.".idapro/plugins/vulfi.py".source = "${vulfi}/vulfi.py";
         file.".idapro/plugins/vulfi_prototypes.json".source = "${vulfi}/vulfi_prototypes.json";
+        file.".idapro/plugins/wakatime.py".source = "${ida-wakatime}/wakatime.py";
 
         activation.ida-pro-mcp = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           ${pkgs.ida-pro-mcp}/bin/ida-pro-mcp --install 2>/dev/null || true
