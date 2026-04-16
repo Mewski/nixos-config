cc74b3eb24859001856d2cb306f0557e7df92905
Mewski <mewski@mewski.dev>
2026-04-16 15:16:46 -0500

    fix: correct greetd

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -3,13 +3,17 @@
     { config, lib, ... }:
     let
       primaryUser = builtins.head (builtins.attrNames config.home-manager.users);
+      session = {
+        user = primaryUser;
+        command = config.desktop.session.command;
+      };
     in
     lib.mkIf (config.desktop.session.command != null) {
       services.greetd = {
         enable = true;
-        settings.initial_session = {
-          user = primaryUser;
-          command = config.desktop.session.command;
+        settings = {
+          default_session = session;
+          initial_session = session;
         };
       };
     };
