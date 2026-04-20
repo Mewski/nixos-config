88d6e8e7ad36d29f5a6ef28b0d3692291f486758
Mewski <mewski@mewski.dev>
2026-04-20 14:25:30 -0500

    fix: correct rstudio

--- modules/overlays/rstudio.nix
+++ modules/overlays/rstudio.nix
@@ -3,6 +3,7 @@
     { pkgs, ... }:
     {
       overlayAttrs.rstudio = pkgs.rstudio.override {
+        nodejs-slim = pkgs.nodejs;
         electron_38 = pkgs.electron_38.overrideAttrs (old: {
           meta = old.meta // {
             knownVulnerabilities = [ ];
